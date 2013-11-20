9e9f0cbb97ffeede1283bcebf66c62aa81f77074
AgentTroll <woodyc40@gmail.com>
2013-11-20 00:03:02 -0800

    Update TheSurvivalGames.java

--- src/main/java/me/theepicbutterstudios/thesurvivalgames/TheSurvivalGames.java
+++ src/main/java/me/theepicbutterstudios/thesurvivalgames/TheSurvivalGames.java
@@ -1,11 +1,13 @@
 package me.theepicbutterstudios.thesurvivalgames;
 
+import com.gmail.woodyc40.arenaapi.*;
+
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TheSurvivalGames extends JavaPlugin  {
 
     public void onEnable() {
-
+        new ArenaManager(this);
     }
 
     public void onDisable() {
