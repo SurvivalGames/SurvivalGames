897f481c4725f21c12c694bafb1c9e3a6789703c
AgentTroll <woodyc40@gmail.com>
2013-11-19 18:06:43 -0800

    Create TheSurvivalGames.java
    I looked inside your InstaTNT plugin and noticed that the package contains capitalized letters, I made them lowercase to follow java conventions.

---
+++ src/main/java/me/theepicbutterstudios/thesurvivalgames/TheSurvivalGames.java
@@ -0,0 +1,15 @@
+package me.theepicbutterstudios.thesurvivalgames;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TheSurvivalGames extends JavaPlugin  {
+
+    public void onEnable() {
+
+    }
+
+    public void onDisable() {
+
+    }
+
+}
