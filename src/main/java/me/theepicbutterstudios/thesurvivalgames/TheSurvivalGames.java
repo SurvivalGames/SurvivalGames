267778752ab6a6a9728e28ddd2a422bbb05d2669
AgentTroll <woodyc40@gmail.com>
2013-11-23 12:02:50 -0800

    Update TheSurvivalGames.java

--- src/main/java/me/theepicbutterstudios/thesurvivalgames/TheSurvivalGames.java
+++ src/main/java/me/theepicbutterstudios/thesurvivalgames/TheSurvivalGames.java
@@ -1,21 +1,22 @@
 /**
  * Name: TheSurvivalGames.java
  * Created: 19 Nov 2013
- * 
+ * Edited 23 Nov 2013
  *
  * @version 1.0.0
  */
 
 package me.theepicbutterstudios.thesurvivalgames;
 
-import com.gmail.woodyc40.arenaapi.*;
 import com.java.util.logging.Logger;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class TheSurvivalGames extends JavaPlugin  {
 
     public void onEnable() {
+        registerAll();
         new ArenaManager(this);
+
         getLogger().info("[The Survival Games] has been enabled!")
         getLogger().info("[The Survival Games] is a community project, join at http://dev.bukkit.org/bukkit-plugins/the-survival-games/");
         saveDefaultConfig();
@@ -25,13 +26,17 @@ public class TheSurvivalGames extends JavaPlugin  {
         getLogger().info("[The Survival Games] was disabled.")
 
     }
-    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
-    {
-        if(commandLabel.equalsIgnoreCase("sg")
-        {
-            //Do Stuff
-        }
-        return true;
+
+    public void registerAll() {
+        //register all commands and listeners
+        getCommand("sg").setExecutor(new CommandHandler());
     }
 
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
+        if(commandLabel.equalsIgnoreCase("sg") {
+            //Do Stuff
+            return true;
+        }
+        return false;
+    }
 }
