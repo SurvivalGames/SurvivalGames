9ddeed7396da9bf9559992ccb4be760ded73f3ed
AgentTroll <woodyc40@gmail.com>
2013-11-23 14:10:38 -0800

    Update SubCommand.java

--- src/main/java/me/theepicbutterstudios/thesurvivalgames/command/SubCommand.java
+++ src/main/java/me/theepicbutterstudios/thesurvivalgames/command/SubCommand.java
@@ -1,6 +1,14 @@
 package me.theepicbutterstudios.thesurvivalgames.command;
 
 public interface SubCommand {
+
+    /**
+     * The method to execute the command. DO NOT CALL. Use only in CommandHandler/override in SubCommands
+     *
+     * @param cmd The name of the command to execute
+     * @param p The player executing the command
+     * @param args The arguments after the command
+     */
     
     public void execute(String cmd, Player p, String[] args);
 
