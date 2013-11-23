7847597fa46026cfee3ca4a2e662267532f8466c
AgentTroll <woodyc40@gmail.com>
2013-11-23 11:56:32 -0800

    Create SGArena.java

---
+++ src/main/java/me/theepicbutterstudios/thesurvivalgames/SGArena.java
@@ -0,0 +1,43 @@
+package com.gmail.woodyc40.arenaapi;
+
+import org.bukkit.Location;
+ 
+import java.util.ArrayList;
+import java.util.List;
+ 
+public class Arena {
+ 
+    public int id = 0;
+    Location spawn = null;
+    List<String> players = new ArrayList<String>();
+ 
+    /**
+     * Constructs a new arena based off of a Location and an ID
+     * 
+     * @param id The ID the arena will have
+     */
+    
+    public Arena(int id) {
+        this.id = id;
+    }
+ 
+    /**
+     * Gets the ID of the arena
+     * 
+     * @return The ID of the arena
+     */
+    
+    public int getId() {
+        return this.id;
+    }
+
+    /**
+     * Gets the list of players in the arena
+     * 
+     * @return List of players in the arena
+     */
+    
+    public List<String> getPlayers() {
+        return this.players;
+    }
+}
