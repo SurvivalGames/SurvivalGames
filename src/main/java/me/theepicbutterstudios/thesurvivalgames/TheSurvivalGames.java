a8d17539ecc1ab37a1f8df18eaf90aa3d28bbdab
TheEpicButterStudios <samuel.bird4215@gmail.com>
2013-11-23 07:58:47 -0600

    Update TheSurvivalGames.java

--- src/main/java/me/theepicbutterstudios/thesurvivalgames/TheSurvivalGames.java
+++ src/main/java/me/theepicbutterstudios/thesurvivalgames/TheSurvivalGames.java
@@ -17,12 +17,21 @@ public class TheSurvivalGames extends JavaPlugin  {
     public void onEnable() {
         new ArenaManager(this);
         getLogger().info("[The Survival Games] has been enabled!")
-        getLogger().info("[The Survival Games] is a community project, join at http://dev.bukkit.org/bukkit-plugins/the-survival-games/")
+        getLogger().info("[The Survival Games] is a community project, join at http://dev.bukkit.org/bukkit-plugins/the-survival-games/");
+        saveDefaultConfig();
     }
 
     public void onDisable() {
         getLogger().info("[The Survival Games] was disabled.")
 
+    }
+    public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args)
+    {
+        if(commandLabel.equalsIgnoreCase("sg")
+        {
+            //Do Stuff
+        }
+        return true;
     }
 
 }
