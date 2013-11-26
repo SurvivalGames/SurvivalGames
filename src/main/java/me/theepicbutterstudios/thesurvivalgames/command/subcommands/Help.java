376c803966efaa285539e82bfd3a9f9af5a3ee0a
AgentTroll <woodyc40@gmail.com>
2013-11-25 23:36:02 -0800

    Update Help.java

--- src/main/java/me/theepicbutterstudios/thesurvivalgames/command/subcommands/Help.java
+++ src/main/java/me/theepicbutterstudios/thesurvivalgames/command/subcommands/Help.java
@@ -1,3 +1,10 @@
+/**
+ * Name: Help.java
+ * Edited: 25 November 2013
+ *
+ * @version 1.0.0
+ */
+
 package me.theepicbutterstudios.thesurvivalgames.command.subcommands;
 
 import me.theepicbutterstudios.thesurvivalgames.command.SubCommand;
