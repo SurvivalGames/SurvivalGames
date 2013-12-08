ab8cbc40382c4e60613cfde8bfffc109d82b3772
Dylan Ryman <dylan@dtr-world.com>
2013-12-08 14:27:44 -0500

    Added arena states

---
+++ src/main/java/me/theepicbutterstudios/thesurvivalgames/ArenaState.java
@@ -0,0 +1,12 @@
+package me.theepicbutterstudios.thesurvivalgames;
+
+/**
+ * Name: ArenaState.java
+ * Edited: 8 December 2013
+ *
+ * @version 1.0.0
+ */
+
+public enum ArenaState {
+	WAITING_FOR_PLAYERS, STARTING_COUNTDOWN, PRE_GAME, IN_GAME, POST_GAME
+}
