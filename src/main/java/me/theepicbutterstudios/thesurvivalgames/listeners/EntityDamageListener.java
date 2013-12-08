a272f01b8e8ae455350e219ff8af21b8774d5592
AgentTroll <woodyc40@gmail.com>
2013-12-07 22:48:18 -0800

    Update EntityDamageListener.java

--- src/main/java/me/theepicbutterstudios/thesurvivalgames/listeners/EntityDamageListener.java
+++ src/main/java/me/theepicbutterstudios/thesurvivalgames/listeners/EntityDamageListener.java
@@ -1,7 +1,5 @@
 package me.theepicbutterstudios.thesurvivalgames.listeners;
 
-import me.theepicbutterstudios.thesurvivalgames.TheSurvivalGames;
-
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Snowball;
@@ -14,14 +12,9 @@ import org.bukkit.potion.PotionEffectType;
 
 public class EntityDamageListener implements Listener {
 
-	TheSurvivalGames plugin;
-
-	public EntityDamageListener(TheSurvivalGames plugin) {
-		this.plugin = plugin;
-	}
-
 	/**
 	 * Listens for a player being hit by a snow ball, gives player Slowness II for 30 seconds
+         *
 	 * @param event - The EntityDamageByEntityEvent event
 	 */
 	@EventHandler(priority = EventPriority.HIGHEST)
@@ -30,7 +23,7 @@ public class EntityDamageListener implements Listener {
 		if (entity instanceof Snowball) {
 			if (event.getEntity() instanceof Player) {
 				Player damaged = (Player) event.getEntity();
-				if (plugin.getArenaManager().isInGame(damaged)) {
+				if (ArenaManager().getManager().isInGame(damaged)) {
 					damaged.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 600, 2, false));
 				}
 			}
