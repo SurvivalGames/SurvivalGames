afa56e2fb3190d27d8197ce8a071169643d9677c
AgentTroll <woodyc40@gmail.com>
2013-12-08 16:23:37 -0800

    Update Countdown.java

--- src/main/java/me/theepicbutterstudios/thesurvivalgames/runnables/Countdown.java
+++ src/main/java/me/theepicbutterstudios/thesurvivalgames/runnables/Countdown.java
@@ -10,14 +10,33 @@ public class Countdown implements Runnable {
     int count = 0;
     int id = 0;
     
+    /**
+     * Constructs a new countdown for this arena
+     * 
+     * @param a The arena to countdown on
+     * @param count The amount of numbers to countdown from
+     */ 
+    
     public Countdown(SGArena a, int count) {
         this.a = a;
         this.count = count;
     }
     
+    /**
+     * Sets the id for the runnable to cancel
+     * 
+     * @param id The return value from starting a new task
+     */
+     
     public void setId(int id) {
         this.id = id;
     }
+
+    /**
+     * 
+     * The overrided method to run
+     * 
+     */ 
 
     @Override
     public void run() {
