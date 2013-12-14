a92bab8e24701a06c90d567dfbacabf5416fc360
AgentTroll <woodyc40@gmail.com>
2013-12-13 22:35:08 -0800

    Setup mutilworld

---
+++ src/main/java/me/theepicbutterstudios/thesurvivalgames/multiworld/MultiworldMain.java
@@ -0,0 +1,11 @@
+/**
+ * Name: MultiworldMain.java
+ * Created: 13 December 2013
+ *
+ * @version 1.0.0
+ */
+package me.theepicbutterstudios.thesurvivalgames.multiworld;
+
+public class MultiworldMain {
+
+}
