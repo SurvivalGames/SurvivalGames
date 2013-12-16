514921bef584451d141bf158e9a64db909bb7a3d
AgentTroll <woodyc40@gmail.com>
2013-12-16 06:29:02 -0800

    Create SGWorld.java

---
+++ src/main/java/me/theepicbutterstudios/thesurvivalgames/multiworld/SGWorld.java
@@ -0,0 +1,34 @@
+/**
+ * Name: SGWorld.java
+ * Created: 16 December 2013
+ *
+ * @version 1.0.0
+ */
+package me.theepicbutterstudios.thesurvivalgames.multiworld;
+
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.WorldCreator;
+import org.bukkit.WorldType;
+
+public class World {
+
+    String name;
+    WorldCreator wc;
+
+    public World(String name) {
+        if(Bukkit.getServer().getWorld(name) != null) 
+            return;
+
+        this.name = name;
+
+        wc = new WorldCreator(name);
+        wc.enviroment(World.Enviroment.NORMAL);
+        wc.type(WorldType.NORMAL);
+    }
+
+    public void create() {
+        wc.createWorld();
+    }
+
+}
