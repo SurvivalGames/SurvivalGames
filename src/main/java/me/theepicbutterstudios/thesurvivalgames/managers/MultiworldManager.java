27ccd1b1fdbda725d1ef88614855a00022e58880
AgentTroll <woodyc40@gmail.com>
2013-12-16 16:17:48 -0800

    Nice!

---
+++ src/main/java/me/theepicbutterstudios/thesurvivalgames/managers/MultiworldManager.java
@@ -0,0 +1,61 @@
+/**
+ * Name: MultiworldMain.java
+ * Created: 13 December 2013
+ *
+ * @version 1.0.0
+ */
+package me.theepicbutterstudios.thesurvivalgames.managers;
+
+import me.theepicbutterstudios.thesurvivalgames.multiworld.SGWorld;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+
+import java.io.*;
+import java.net.URL;
+
+public class MultiworldManager {
+
+    static final MultiworldManager wm = new MultiworldManager();
+
+    public MultiworldManager() {}
+
+    public static MultiworldManager getInstance() {
+        return wm;
+    }
+
+    public World createWorld(String name) {
+        return new SGWorld(name).create();
+    }
+
+    public void deleteWorld(String name) {
+        new SGWorld(name).remove();
+    }
+
+    public World copyFromInternet(String url) throws IOException {
+        URL source = new URL(url);
+        if(url == null) {
+            return null;
+        }
+
+        File target = null;
+        int i = 0;
+        while(true) {
+            String name = "SurvivalGamesWorld" + i;
+            if(Bukkit.getServer().getWorld(name) == null) {
+                target = createWorld(name).getWorldFolder();
+                break;
+            }
+        }
+
+        InputStream in = source.openConnection().getInputStream();
+        OutputStream out = new FileOutputStream(target);
+        byte[] buffer = new byte[1024];
+        int length;
+        while ((length = in.read(buffer)) > 0)
+            out.write(buffer, 0, length);
+        in.close();
+        out.close();
+
+        return Bukkit.getServer().getWorld("SurvivalGamesWorld" + i);
+    }
+}
--- src/main/java/me/theepicbutterstudios/thesurvivalgames/multiworld/SGWorld.java
+++ src/main/java/me/theepicbutterstudios/thesurvivalgames/multiworld/SGWorld.java
@@ -6,31 +6,66 @@
  */
 package me.theepicbutterstudios.thesurvivalgames.multiworld;
 
-import org.bukkit.Bukkit;
-import org.bukkit.World;
-import org.bukkit.WorldCreator;
-import org.bukkit.WorldType;
+import org.bukkit.*;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
 
-public class World {
+import java.io.File;
 
-    String name;
-    WorldCreator wc;
+public class SGWorld {
 
-    public World(String name) {
-        if(Bukkit.getServer().getWorld(name) != null) 
-            return;
+    private final String name;
+    private final WorldCreator wc;
 
+    public SGWorld(String name) {
         this.name = name;
 
         wc = new WorldCreator(name);
-        wc.enviroment(World.Enviroment.NORMAL);
+        wc.environment(World.Environment.NORMAL);
         wc.type(WorldType.NORMAL);
     }
 
-    public void create() {
-        wc.createWorld();
+    public World getWorld() {
+        return name != null ? Bukkit.getServer().getWorld(name) : null;
     }
 
-    public void remove() {}
+    public World create() {
+        if(Bukkit.getServer().getWorld(name) != null) {
+            //TODO What do we do? Should we copy the world? Or skip this step?
+            return Bukkit.getServer().getWorld(name);
+        }
+
+        return wc.createWorld();
+    }
+
+    public void remove() {
+        World world = Bukkit.getServer().getWorld(name);
+
+        for(Player p : world.getPlayers()) {
+            //TODO teleport player somewhere safe
+        }
+        for(Entity e : world.getEntities()) {
+            e.remove();
+        }
+        for(Chunk c : world.getLoadedChunks()) {
+            c.unload(false, false);
+            world.unloadChunk(c);
+        }
+        Bukkit.getServer().unloadWorld(world, false);
+        deleteFiles(world.getWorldFolder());
+    }
+
+    private void deleteFiles(File path) {
+        if (path.exists()) {
+            File files[] = path.listFiles();
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    deleteFiles(file);
+                } else {
+                    file.delete();
+                }
+            }
+        }
+    }
 
 }
