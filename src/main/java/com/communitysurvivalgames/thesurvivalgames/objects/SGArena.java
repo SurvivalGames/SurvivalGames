796b5126fd3c3338de568b4b848614c0d176dd35
Dylan Ryman <dylan@dtr-world.com>
2013-12-19 16:36:00 -0500

    Let's try and not break the CI server, @Pik0Dev

--- src/main/java/com/communitysurvivalgames/thesurvivalgames/objects/SGArena.java
+++ src/main/java/com/communitysurvivalgames/thesurvivalgames/objects/SGArena.java
@@ -23,7 +23,6 @@ public class SGArena {
     public int maxPlayers;
     public int minPlayers;
     List<String> players = new ArrayList<String>();
-    public Scoreboard scoreboard;
 
     /**
      * Name: ArenaState.java Edited: 8 December 2013
