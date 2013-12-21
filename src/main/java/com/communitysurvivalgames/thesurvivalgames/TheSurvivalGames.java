259a00a9e7971be27da17cb84cdca1c99fc33b14
Dylan Ryman <dylan@dtr-world.com>
2013-12-21 13:45:09 -0500

    Added base I18N language support

--- src/main/java/com/communitysurvivalgames/thesurvivalgames/TheSurvivalGames.java
+++ src/main/java/com/communitysurvivalgames/thesurvivalgames/TheSurvivalGames.java
@@ -11,7 +11,8 @@ import java.util.List;
 
 import javax.persistence.PersistenceException;
 
-
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -31,6 +32,7 @@ import com.communitysurvivalgames.thesurvivalgames.command.subcommands.party.Pro
 import com.communitysurvivalgames.thesurvivalgames.listeners.EntityDamageListener;
 import com.communitysurvivalgames.thesurvivalgames.listeners.ItemListener;
 import com.communitysurvivalgames.thesurvivalgames.listeners.SetupListener;
+import com.communitysurvivalgames.thesurvivalgames.local.I18N;
 import com.communitysurvivalgames.thesurvivalgames.managers.ArenaManager;
 import com.communitysurvivalgames.thesurvivalgames.objects.PlayerData;
 import com.communitysurvivalgames.thesurvivalgames.runnables.Scoreboard;
@@ -38,100 +40,110 @@ import com.communitysurvivalgames.thesurvivalgames.util.DoubleJump;
 
 public class TheSurvivalGames extends JavaPlugin {
 
-    static TheSurvivalGames plugin;
+	static TheSurvivalGames plugin;
 
-    @Override
-    public void onEnable() {
-        plugin = this;
-        registerAll();
-        setupDatabase();
-        ArenaManager am = new ArenaManager(this);
-        am.loadGames();
+	@Override
+	public void onEnable() {
+		plugin = this;
 
-        getLogger().info("has been enabled!");
-        getLogger().info("is a community project, join at http://dev.bukkit.org/bukkit-plugins/the-survival-games/");
-        saveDefaultConfig();
-    }
+		File i18N = new File(getDataFolder(), "I18N.yml");
+		if (!i18N.exists()) {
+			saveResource("I18N.yml", false);
+		}
+		FileConfiguration users = YamlConfiguration.loadConfiguration(i18N);
+		
+		I18N.setupLocale();
+		I18N.setLocale(users.getString("enUS"));
 
-    @Override
-    public void onDisable() {
-        getLogger().info("was disabled.");
-    }
+		registerAll();
+		setupDatabase();
+		ArenaManager am = new ArenaManager(this);
+		am.loadGames();
 
-    public void registerAll() {
-        // register all commands and listeners
-        getCommand("sg").setExecutor(new CommandHandler());
-        getCommand("party").setExecutor(new PartyCommandHandler());
+		getLogger().info("has been enabled!");
+		getLogger().info("is a community project, join at http://dev.bukkit.org/bukkit-plugins/the-survival-games/");
+		saveDefaultConfig();
+	}
 
-        CommandHandler.register("help", new HelpCommand());
-        CommandHandler.register("create", new CreateCommand());
-        CommandHandler.register("join", new JoinCommand());
-        CommandHandler.register("user", new UserCommand());
+	@Override
+	public void onDisable() {
+		getLogger().info("was disabled.");
+	}
 
-        PartyCommandHandler.register("chat", new ChatCommand());
-        PartyCommandHandler.register("decline", new DeclineCommand());
-        PartyCommandHandler.register("help", new HelpCommand());
-        PartyCommandHandler.register("invite", new InviteCommand());
-        PartyCommandHandler.register("join", new JoinCommand());
-        PartyCommandHandler.register("leave", new LeaveCommand());
-        PartyCommandHandler.register("list", new ListCommand());
-        PartyCommandHandler.register("promote", new PromoteCommand());
+	public void registerAll() {
+		// register all commands and listeners
+		getCommand("sg").setExecutor(new CommandHandler());
+		getCommand("party").setExecutor(new PartyCommandHandler());
 
-        PluginManager pm = getServer().getPluginManager();
+		CommandHandler.register("help", new HelpCommand());
+		CommandHandler.register("create", new CreateCommand());
+		CommandHandler.register("join", new JoinCommand());
+		CommandHandler.register("user", new UserCommand());
 
-        pm.registerEvents(new ItemListener(this), this);
-        pm.registerEvents(new SetupListener(), this);
-        pm.registerEvents(new EntityDamageListener(), this);
-        pm.registerEvents(new DoubleJump(this), this);
+		PartyCommandHandler.register("chat", new ChatCommand());
+		PartyCommandHandler.register("decline", new DeclineCommand());
+		PartyCommandHandler.register("help", new HelpCommand());
+		PartyCommandHandler.register("invite", new InviteCommand());
+		PartyCommandHandler.register("join", new JoinCommand());
+		PartyCommandHandler.register("leave", new LeaveCommand());
+		PartyCommandHandler.register("list", new ListCommand());
+		PartyCommandHandler.register("promote", new PromoteCommand());
 
-        Scoreboard.registerScoreboard(this);
-    }
+		PluginManager pm = getServer().getPluginManager();
 
-    /**
-     * Setup Persistence Databases and Install DDL if there are none
-     */
-    private void setupDatabase() {
-        File ebean = new File(getDataFolder(), "ebean.properties");
-        if (!ebean.exists()) {
-            saveResource("ebean.properties", false);
-        }
-        try {
-            getDatabase().find(PlayerData.class).findRowCount();
-        } catch (PersistenceException ex) {
-            System.out.println("Installing database for " + getDescription().getName() + " due to first time usage");
-            installDDL();
-        }
-    }
+		pm.registerEvents(new ItemListener(this), this);
+		pm.registerEvents(new SetupListener(), this);
+		pm.registerEvents(new EntityDamageListener(), this);
+		pm.registerEvents(new DoubleJump(this), this);
 
-    /**
-     * Gets Persistence Database classes WARNING: DO NOT EDIT
-     * 
-     * @return The list of classes for the database
-     */
-    @Override
-    public List<Class<?>> getDatabaseClasses() {
-        List<Class<?>> list = new ArrayList<Class<?>>();
-        list.add(PlayerData.class);
-        return list;
-    }
+		Scoreboard.registerScoreboard(this);
+	}
 
-    public PlayerData getPlayerData(Player player) {
-        PlayerData data = getDatabase().find(PlayerData.class).where().ieq("playerName", player.getName()).findUnique();
-        if (data == null) {
-            data = new PlayerData(player);
-        }
+	/**
+	 * Setup Persistence Databases and Install DDL if there are none
+	 */
+	private void setupDatabase() {
+		File ebean = new File(getDataFolder(), "ebean.properties");
+		if (!ebean.exists()) {
+			saveResource("ebean.properties", false);
+		}
+		try {
+			getDatabase().find(PlayerData.class).findRowCount();
+		} catch (PersistenceException ex) {
+			System.out.println("Installing database for " + getDescription().getName() + " due to first time usage");
+			installDDL();
+		}
+	}
 
-        return data;
-    }
+	/**
+	 * Gets Persistence Database classes WARNING: DO NOT EDIT
+	 * 
+	 * @return The list of classes for the database
+	 */
+	@Override
+	public List<Class<?>> getDatabaseClasses() {
+		List<Class<?>> list = new ArrayList<Class<?>>();
+		list.add(PlayerData.class);
+		return list;
+	}
 
-    public void setPlayerData(PlayerData data) {
-        getDatabase().save(data);
-    }
+	public PlayerData getPlayerData(Player player) {
+		PlayerData data = getDatabase().find(PlayerData.class).where().ieq("playerName", player.getName()).findUnique();
+		if (data == null) {
+			data = new PlayerData(player);
+		}
 
-    /**
-     * Only to be used if 100% needed
-     */
-    public static TheSurvivalGames getPlugin() {
-        return plugin;
-    }
+		return data;
+	}
+
+	public void setPlayerData(PlayerData data) {
+		getDatabase().save(data);
+	}
+
+	/**
+	 * Only to be used if 100% needed
+	 */
+	public static TheSurvivalGames getPlugin() {
+		return plugin;
+	}
 }
---
+++ src/main/java/com/communitysurvivalgames/thesurvivalgames/local/I18N.java
@@ -0,0 +1,119 @@
+package com.communitysurvivalgames.thesurvivalgames.local;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Properties;
+import java.util.logging.Level;
+
+import org.bukkit.Bukkit;
+
+import com.communitysurvivalgames.thesurvivalgames.TheSurvivalGames;
+
+public class I18N {
+	private static Properties locales = new Properties();
+	private static Properties fallback = new Properties();
+	private static File dir = new File(TheSurvivalGames.getPlugin().getDataFolder(), "locale");
+	public static HashMap<String, String> localeFiles = new HashMap<String, String>();
+	public static HashMap<Integer, String> localeIndices = new HashMap<Integer, String>();
+	public static Locale currentLocale = Locale.enUS;
+
+	public enum Locale {
+		enUS, idID
+	}
+
+	/**
+	 * Gets the locale properties and stores loads it to locales
+	 * @param file The locale file
+	 */
+	private static void getLocaleProperties(String file) {
+		locales.clear();
+		if (file.equalsIgnoreCase("enUS")) {
+			try {
+				locales.load(new InputStreamReader(I18N.class.getResource("/i18n/enUS").openStream(), "UTF8"));
+			} catch (IOException e) {
+				Bukkit.getLogger().log(Level.SEVERE, "[i18n] Could not load language file", e);
+			}
+		} else {
+			try {
+				locales.load(new InputStreamReader(new FileInputStream(dir.getAbsolutePath() + File.separator + file), "UTF8"));
+			} catch (IOException e) {
+				Bukkit.getLogger().log(Level.SEVERE, "[i18n] Could not load language file", e);
+			}
+		}
+	}
+
+	/**
+	 * Set available locales and load fallback locale
+	 */
+	public static void setupLocale() {
+		localeFiles.put("enUS", "English");
+		localeIndices.put(0, "enUS");
+		addFiles();
+	}
+
+	/**
+	 * Add files from the locale directory
+	 */
+	public static void addFiles() {
+		int i = 1;
+		Properties tmp = new Properties();
+		String[] list = dir.list();
+		for (String file : list) {
+			if (file.matches("^\\w{4}$")) {
+				try {
+					if (!file.equalsIgnoreCase("enUS")) {
+						tmp.clear();
+						tmp.load(new InputStreamReader(new FileInputStream(dir.getAbsolutePath() + File.separator + file), "UTF8"));
+						localeFiles.put(file, tmp.getProperty("LOCALE_NAME", file));
+						localeIndices.put(i, file);
+						i++;
+					}
+				} catch (IOException e) {
+					Bukkit.getLogger().log(Level.SEVERE, "[i18n] Could not load language file", e);
+				}
+			}
+		}
+		try {
+			fallback.clear();
+			fallback.load(new InputStreamReader(I18N.class.getResource("/i18n/enUS").openStream(), "UTF8"));
+			Bukkit.getLogger().log(Level.SEVERE, "[i18n] Fallback enUS loaded");
+		} catch (IOException e) {
+			Bukkit.getLogger().log(Level.SEVERE, "[i18n] Could not load fallback file", e);
+		}
+	}
+
+	/**
+	 * Sets the locale for the launcher
+	 * @param locale the language file to be loaded
+	 */
+	public static void setLocale(String locale) {
+		if (locale.equalsIgnoreCase("idID")) {
+			currentLocale = Locale.idID;
+		} else {
+			currentLocale = Locale.enUS;
+		}
+		getLocaleProperties(locale);
+		Bukkit.getLogger().log(Level.INFO, "[i18n] " + locale + " " + locales.getProperty("LOCALE_LOADED", "loaded"));
+	}
+
+	/**
+	 * Gets the default translation for the key (enUS)
+	 * @param key The key for the string
+	 * @return the default string
+	 */
+	private static String getFallbackString(String key) {
+		return fallback.getProperty(key, key);
+	}
+
+	/**
+	 * Gets the localized string for the field, if not defined, returns the key
+	 * @param key The key for the string
+	 * @return The localized string or fallback value
+	 */
+	public static String getLocaleString(String key) {
+		return locales.getProperty(key, getFallbackString(key));
+	}
+}
