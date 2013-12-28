9b4689c00d50c9bb87fb2cdde562f6b930264cb2
AgentTroll <woodyc40@gmail.com>
2013-12-28 13:56:36 -0800

    Update TimeManager.java

--- src/main/java/com/communitysurvivalgames/thesurvivalgames/managers/TimeManager.java
+++ src/main/java/com/communitysurvivalgames/thesurvivalgames/managers/TimeManager.java
@@ -7,6 +7,7 @@
 package com.communitysurvivalgames.thesurvivalgames.managers;
 
 import com.communitysurvivalgames.thesurvivalgames.TheSurvivalGames;
+import com.communitysurvivalgames.thesurvivalgames.listeners.*;
 import com.communitysurvivalgames.thesurvivalgames.object.SGArena;
 import com.communitysurvivalgames.thesurvivalgames.runnables.*;
 
@@ -27,24 +28,61 @@ public class TimeManager {
         Countdown c = new Countdown(a, 1, 5, "Game", "minutes", new CodeExecutor {
             a.broadcast(I18N.getLocaleString("GAME_STARTING"));
             a.setState(SGArena.ArenaState.STARTING_COUNTDOWN);
-            for(org.bukkit.Location loc : a.locs) {
-                for(String s : a.getPlayers()) {
-                    org.bukkit.entity.Player p = Bukkit.getPlayerExact(s);
-                    if(p != null) {
-                        p.teleport(loc);
-                        continue;
-                    }
-                }
-                coontinue; //TODO gotta figure this out...
+            for(int i = 0; i <= a.maxplayers; i++) {
+                org.bukkit.entity.Player p = Bukkit.getPlayerExact(a.getPlayers().get(i));
+                org.bukkit.Location loc = a.locs.get(i);
+                p.teleport(loc);  
+                MoveListener.getPlayers().add(a.getPlayers().get(i));
+                
+                countdown();
             }
-        }
+        });
         c.setId(Bukkit.getScheduler().runTaskTimer(TheSurvivalGames.getPlugin(), c, 0L, 60*20L);
     }
 
-    public void countdownGame() {
-        Countdown c = new Countdown(a, 5, 5, "DeathMatch", "minutes", new CodeExecutor {
-            //TODO deathmatch, tp players.
-        }
+    public void countdown() {
+        Countdown c = new Countdown(a, 1, 10, "Game", "seconds", new CodeExecutor {
+            a.broadcast(I18N.getLocaleString("ODDS"));
+            a.setState(SGArena.ArenaState.IN_GAME);
+            for(String s : a.getPlayers()) {
+                if(MoveListener.getPlayers().contains(s)) {
+                    MoveListener.getPlayers().remove(s);
+                }
+            }
+            
+            countdownDm();
+        });
+        c.setId(Bukkit.getScheduler().runTaskTimer(TheSurvivalGames.getPlugin(), c, 0L, 20L);
+    }
+    
+    public void countdownDm() {
+        Countdown c = new Countdown(a, 5, 30, "DeathMatch", "minutes", new CodeExecutor {
+            a.broadcast(I18N.getLocaleString("DM_STARTING"));
+            a.setState(SGArena.ArenaState.DEATHMATCH);
+            SafeEntityListener.getPlayers().addAll(a.getPlayers());
+            //tp to deathmatch
+            
+            commenceDm();
+        });
         c.setId(Bukkit.getScheduler().runTaskTimer(TheSurvivalGames.getPlugin(), c, 0L, 5*60*20L);
     }
+    
+    public void commenceDm() {
+        Countdown c = new Countdown(a, 1, 10, "DeathMatch", "seconds", new CodeExecutor {
+            a.broadcast(I18N.getLocaleString("START"));
+            SafeEntityListener.getPlayers().removeAll(a.getPlayers());
+            
+            countdownEnd();
+        });
+        c.setId(Bukkit.getScheduler().runTaskTimer(TheSurvivalGames.getPlugin(), c, 0L, 20L);
+    }
+    
+    public void countdownEnd() {
+        Countdown c = new Countdown(a, 1, 5, "EndGame", "minutes", new CodeExecutor {
+            a.broadcast(I18N.getLocaleString("END") + " TIED_GAME");
+            //tp out of arena, rollback, pick up all items and arrows
+        });
+        c.setId(Bukkit.getScheduler().runTaskTimer(TheSurvivalGames.getPlugin(), c, 0L, 60*20L);
+    }
+    
 }
