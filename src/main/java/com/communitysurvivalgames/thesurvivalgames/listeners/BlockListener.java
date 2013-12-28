6e55150138dfd81d9f2f971ea22aadbe76fa4b6a
AgentTroll <woodyc40@gmail.com>
2013-12-28 13:57:17 -0800

    Update BlockListener.java

--- src/main/java/com/communitysurvivalgames/thesurvivalgames/listeners/BlockListener.java
+++ src/main/java/com/communitysurvivalgames/thesurvivalgames/listeners/BlockListener.java
@@ -1,5 +1,6 @@
 /**
- * Name: TnT.java Created: 9 December 2013
+ * Name: BlockListener.java 
+ * Created: 9 December 2013
  *
  * @version 1.0.0
  */
@@ -8,12 +9,11 @@ package com.communitysurvivalgames.thesurvivalgames.listeners;
 import com.communitysurvivalgames.thesurvivalgames.managers.ArenaManager;
 import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
+import org.bukkit.event.*;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 
-class BlockListener {
+public class BlockListener implements Listener{
 
     @EventHandler(priority = EventPriority.MONITOR)
     public void onBlockPlace(BlockPlaceEvent event) {
