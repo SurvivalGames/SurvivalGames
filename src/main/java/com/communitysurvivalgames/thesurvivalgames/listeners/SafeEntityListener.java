c9cc5321ee483b012c5506603a7a619d10b2c591
AgentTroll <woodyc40@gmail.com>
2013-12-28 13:44:03 -0800

    Create SafeEntityListener.java

---
+++ src/main/java/com/communitysurvivalgames/thesurvivalgames/listeners/SafeEntityListener.java
@@ -0,0 +1,33 @@
+/**
+ * Name: SafeEntityListener.java 
+ * Created: 29 December 2013
+ *
+ * @version 1.0.0
+ */
+package com.communitysurvivalgames.thesurvivalgames.listeners;
+
+import org.bukkit.entity.Player;
+import org.bukkit.event.*;
+import org.bukkit.event.entity.EntityDamageEvent;
+
+import java.utili.ArrayList;
+import java.util.List;
+
+public class BlockListener implements Listener {
+
+    public static List<String> safe = new ArrayList<String>();
+
+    @EventHandler
+    public void onDamage(EntityDamageEvent e) {
+        if(e.getEntity() instanceof Player) {
+            if(safe.contains(((Player) e.getEntity()).getName())) {
+                e.setCancelled(true);
+            }
+        }
+    }
+    
+    public static List<String> getPlayers() {
+        return safe;    
+    }
+
+}
