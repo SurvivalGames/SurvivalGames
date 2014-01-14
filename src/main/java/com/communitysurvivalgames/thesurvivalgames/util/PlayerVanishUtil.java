1010a97ae327f7c6a360fd7865ae5e74553a6846
Relicum <chrislutte@relicum.com>
2014-01-14 05:03:33 +0000

    added warning to file I am working on.

--- src/main/java/com/communitysurvivalgames/thesurvivalgames/util/PlayerVanishUtil.java
+++ src/main/java/com/communitysurvivalgames/thesurvivalgames/util/PlayerVanishUtil.java
@@ -5,6 +5,7 @@ import com.communitysurvivalgames.thesurvivalgames.objects.SGArena;
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
+// NOT NOT EDIT THIS FILE RELICUM IS RECODING IT //
 public class PlayerVanishUtil {
 
     public static void hideAll(final SGArena arena, final Player player) {
@@ -22,7 +23,7 @@ public class PlayerVanishUtil {
 
     public static void showAll(final Player player) {
         Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(TheSurvivalGames.getPlugin(TheSurvivalGames.class), new Runnable() {
-          @Override
+            @Override
             public void run() {
                 for (int i = 0; i < Bukkit.getServer().getOnlinePlayers().length; i++) {
                     Bukkit.getServer().getOnlinePlayers()[i].showPlayer(player);
