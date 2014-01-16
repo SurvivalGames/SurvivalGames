de26f4d757653b86a811c77b32f1e459b9c1966b
Dylan Ryman <dylan@dtr-world.com>
2014-01-16 16:51:33 -0500

    Revert "Revert"
    This reverts commit 60a67ac1ea65aae6617f7c3b641115e0f86c94b6.

---
+++ src/main/java/com/communitysurvivalgames/thesurvivalgames/enchantment/DedicationEnchantment.java
@@ -0,0 +1,42 @@
+package com.communitysurvivalgames.thesurvivalgames.enchantment;
+
+import org.bukkit.enchantments.Enchantment;
+import org.bukkit.enchantments.EnchantmentTarget;
+import org.bukkit.inventory.ItemStack;
+
+public class DedicationEnchantment extends Enchantment {
+
+    public DedicationEnchantment(int id) {
+        super(id);
+    }
+
+    @Override
+    public boolean canEnchantItem(ItemStack item) {
+        return true;
+    }
+
+    @Override
+    public boolean conflictsWith(Enchantment other) {
+        return false;
+    }
+
+    @Override
+    public EnchantmentTarget getItemTarget() {
+        return EnchantmentTarget.WEAPON;
+    }
+
+    @Override
+    public int getMaxLevel() {
+        return 1;
+    }
+
+    @Override
+    public String getName() {
+        return "Dedication";
+    }
+
+    @Override
+    public int getStartLevel() {
+        return 1;
+    }
+}
