b0231060c2aa6f7275a3733aa8e3397814b1c21f
AgentTroll <woodyc40@gmail.com>
2014-01-19 21:30:40 -0800

    Update GameStartEvent.java

--- src/main/java/com/communitysurvivalgames/thesurvivalgames/event/GameStartEvent.java
+++ src/main/java/com/communitysurvivalgames/thesurvivalgames/event/GameStartEvent.java
@@ -1,10 +1,17 @@
 package com.communitysurvivalgames.thesurvivalgames.event;
 
+import com.communitysurvivalgames.thesurvivalgames.objects.SGArena;
+
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 
 public class GameStartEvent extends Event {
     private static final HandlerList handlers = new HandlerList();
+    SGArena arena = null;
+    
+    public GameStartEvent(SGArena arena) {
+        this.arena = arena;    
+    }
     
     public HandlerList getHandlers() {
         return handlers;
