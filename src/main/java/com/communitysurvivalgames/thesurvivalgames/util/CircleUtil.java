4cdc273cbce6d61c4cacf028e63cc0686a4d9143
Dylan Ryman <dylan@dtr-world.com>
2014-02-01 19:06:53 -0500

    Added nested runnable class to reduce lines

--- src/main/java/com/communitysurvivalgames/thesurvivalgames/util/CircleUtil.java
+++ src/main/java/com/communitysurvivalgames/thesurvivalgames/util/CircleUtil.java
@@ -4,72 +4,64 @@ import org.bukkit.Bukkit;
 import org.bukkit.FireworkEffect;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
 
 import com.communitysurvivalgames.thesurvivalgames.managers.SGApi;
 
-public class CircleUtil {
-	public static void playFireworkCircle(final Player player, final FireworkEffect effect, int size, final int distance) {
+public class CircleUtil implements Listener {
+
+	static CircleUtil circleUtil = new CircleUtil();
+
+	public static CircleUtil getCircleUtil() {
+		return circleUtil;
+	}
+
+	public void playFireworkCircle(final Player player, final FireworkEffect effect, int size, final int distance) {
 		Bukkit.getLogger().info("Called firework method");
 		int index = 0;
 		for (double t = 0; t < 2 * Math.PI; t += Math.toRadians(size)) {
 			index += 3;
 			Location l = player.getLocation().add(Math.cos(t) * distance, 0.5, Math.sin(t) * distance);
-			final Location fLoc = l;
-			Bukkit.getScheduler().scheduleSyncDelayedTask(SGApi.getPlugin(), new Runnable() {
-				@Override
-				public void run() {
-					Bukkit.getLogger().info("Got location" + fLoc.toString());
-					try {
-						FireworkEffectPlayer.getFireworkEffectPlayer().playFirework(player.getWorld(), fLoc, effect);
-						Bukkit.getLogger().info("Played it!");
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				}
-			}, index);
+			Bukkit.getScheduler().scheduleSyncDelayedTask(SGApi.getPlugin(), new PlayFireworkEffect(player, l, effect), index);
 
 		}
 	}
 
-	public static void playFireworkRing(final Player player, final FireworkEffect effect, int size, final int distance) {
+	public void playFireworkRing(final Player player, final FireworkEffect effect, int size, final int distance) {
 		Bukkit.getLogger().info("Called firework method");
 		int index = 0;
 		for (double t = 0; t < 2 * Math.PI; t += Math.toRadians(size)) {
 			index += 3;
 			Location l = player.getLocation().add(Math.cos(t) * distance, 0.5, Math.sin(t) * distance);
-			final Location fLoc = l;
-			Bukkit.getScheduler().scheduleSyncDelayedTask(SGApi.getPlugin(), new Runnable() {
-				@Override
-				public void run() {
-					Bukkit.getLogger().info("Got location" + fLoc.toString());
-					try {
-						FireworkEffectPlayer.getFireworkEffectPlayer().playFirework(player.getWorld(), fLoc, effect);
-						Bukkit.getLogger().info("Played it!");
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				}
-			}, index);
+			Bukkit.getScheduler().scheduleSyncDelayedTask(SGApi.getPlugin(), new PlayFireworkEffect(player, l, effect), index);
 
 		}
 
 		for (double t = 0; t < 2 * Math.PI; t += Math.toRadians(size)) {
 			index += 3;
 			Location l = player.getLocation().add(Math.cos(t) * distance, 0.5, Math.sin(t) * distance);
-			final Location fLoc = l;
-			Bukkit.getScheduler().scheduleSyncDelayedTask(SGApi.getPlugin(), new Runnable() {
-				@Override
-				public void run() {
-					Bukkit.getLogger().info("Got location" + fLoc.toString());
-					try {
-						FireworkEffectPlayer.getFireworkEffectPlayer().playFirework(player.getWorld(), fLoc, effect);
-						Bukkit.getLogger().info("Played it!");
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				}
-			}, index);
+			Bukkit.getScheduler().scheduleSyncDelayedTask(SGApi.getPlugin(), new PlayFireworkEffect(player, l, effect), index);
+		}
+	}
 
+	public class PlayFireworkEffect implements Runnable {
+		Player player;
+		Location fLoc;
+		FireworkEffect effect;
+
+		public PlayFireworkEffect(Player player, Location fLoc, FireworkEffect fEffect) {
+			this.player = player;
+			this.fLoc = fLoc;
+			this.effect = fEffect;
+		}
+
+		@Override
+		public void run() {
+			try {
+				FireworkEffectPlayer.getFireworkEffectPlayer().playFirework(player.getWorld(), fLoc, effect);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
