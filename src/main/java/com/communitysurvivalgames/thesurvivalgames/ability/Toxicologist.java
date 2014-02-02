48868ea409c489c62e482da813235f043a4caab0
Dylan Ryman <dylan@dtr-world.com>
2014-02-01 19:12:32 -0500

    Fixed another error

--- src/main/java/com/communitysurvivalgames/thesurvivalgames/ability/Toxicologist.java
+++ src/main/java/com/communitysurvivalgames/thesurvivalgames/ability/Toxicologist.java
@@ -35,7 +35,7 @@ public class Toxicologist extends SGAbility implements Listener {
 			final Location loc = new Location(player.getWorld(), player.getLocation().getX(), player.getLocation().getY() + 10, player.getLocation().getZ());
 			final Random rnd = new Random();
 			if (player.getItemInHand().getType() == Material.CLAY_BALL && player.getItemInHand().getItemMeta().getDisplayName().equalsIgnoreCase("Toxen Bomb")) {
-				CircleUtil.playFireworkCircle(event.getPlayer(), FireworkEffect.builder().withColor(Color.SILVER).withFade(Color.GREEN).flicker(true).trail(false).build(), 5, 14);
+				CircleUtil.getCircleUtil().playFireworkCircle(event.getPlayer(), FireworkEffect.builder().withColor(Color.SILVER).withFade(Color.GREEN).flicker(true).trail(false).build(), 5, 14);
 				Bukkit.getScheduler().runTaskLater(SGApi.getPlugin(), new Runnable() {
 
 					@Override
