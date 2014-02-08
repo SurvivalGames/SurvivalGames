49a9c66918352dd13c43afdbc853d8c9430ec014
Dylan Ryman <dylan@dtr-world.com>
2014-02-08 00:02:01 -0500

    Added notch kit (I really like this one (not even OP))

---
+++ src/main/java/com/communitysurvivalgames/thesurvivalgames/ability/Notch.java
@@ -0,0 +1,30 @@
+package com.communitysurvivalgames.thesurvivalgames.ability;
+
+import org.bukkit.Color;
+import org.bukkit.FireworkEffect;
+import org.bukkit.FireworkEffect.Type;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+
+import com.communitysurvivalgames.thesurvivalgames.util.FireworkEffectPlayer;
+
+public class Notch extends SGAbility implements Listener {
+
+	public Notch() {
+		super(7);
+	}
+
+	@EventHandler(priority = EventPriority.HIGH)
+	public void onBlockBreak(BlockBreakEvent event) {
+		if (hasAbility(event.getPlayer())) {
+			FireworkEffect fEffect = FireworkEffect.builder().flicker(false).withColor(Color.BLUE).withFade(Color.AQUA).with(Type.BALL_LARGE).trail(false).build();
+			try {
+				FireworkEffectPlayer.getFireworkEffectPlayer().playFirework(event.getPlayer().getWorld(), event.getPlayer().getLocation(), fEffect);
+			} catch (Exception e) {
+				//If the firework dosen't work... to bad 
+			}
+		}
+	}
+}
