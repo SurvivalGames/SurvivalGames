9e4338a392e7677f5c316a738560379425b57bf4
Dylan Ryman <dylan@dtr-world.com>
2014-02-20 20:11:23 -0500

    User requested feature: Default points for new players

--- src/main/java/com/communitysurvivalgames/thesurvivalgames/listeners/PlayerLoginListener.java
+++ src/main/java/com/communitysurvivalgames/thesurvivalgames/listeners/PlayerLoginListener.java
@@ -1,6 +1,10 @@
 package com.communitysurvivalgames.thesurvivalgames.listeners;
 
 import com.communitysurvivalgames.thesurvivalgames.TheSurvivalGames;
+import com.communitysurvivalgames.thesurvivalgames.managers.SGApi;
+import com.communitysurvivalgames.thesurvivalgames.util.EconUtil;
+
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
@@ -9,19 +13,29 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.inventory.ItemStack;
 
 class PlayerLoginListener implements Listener {
-    private final TheSurvivalGames plugin;
+	private final TheSurvivalGames plugin;
 
-    public PlayerLoginListener(TheSurvivalGames plugin) {
-        this.plugin = plugin;
-    }
+	public PlayerLoginListener(TheSurvivalGames plugin) {
+		this.plugin = plugin;
+	}
 
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        event.setJoinMessage(null);
-        event.getPlayer().getInventory().clear();
-        event.getPlayer().getInventory().setItem(8, new ItemStack(Material.WATCH));
-        for (String s : plugin.getPluginConfig().getWelcomeMessage()) {
-            event.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', s));
-        }
-    }
+	@EventHandler
+	public void onPlayerJoin(final PlayerJoinEvent event) {
+		Bukkit.getScheduler().runTaskLater(SGApi.getPlugin(), new Runnable() {
+
+			@Override
+			public void run() {
+				event.setJoinMessage(null);
+				event.getPlayer().getInventory().clear();
+				event.getPlayer().getInventory().setItem(8, new ItemStack(Material.WATCH));
+				for (String s : plugin.getPluginConfig().getWelcomeMessage()) {
+					event.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', s));
+				}
+				if (!event.getPlayer().hasPlayedBefore()) {
+					EconUtil.addPoints(event.getPlayer(), SGApi.getPlugin().getPluginConfig().getDefaultPoints());
+				}
+			}
+		}, 10L);
+
+	}
 }
