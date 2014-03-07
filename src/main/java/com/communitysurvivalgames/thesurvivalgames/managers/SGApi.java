369f432e9e2d48c9ac61456c1b9dd85bb4d2fb8f
Dylan Ryman <dylan@dtr-world.com>
2014-03-07 15:33:14 -0500

    Made changes to allow for a singleton PartyManager

--- src/main/java/com/communitysurvivalgames/thesurvivalgames/managers/SGApi.java
+++ src/main/java/com/communitysurvivalgames/thesurvivalgames/managers/SGApi.java
@@ -143,6 +143,8 @@ public class SGApi {
 	 * @return the party manager
 	 */
 	public static PartyManager getPartyManager() {
+		if(partyManager == null)
+			partyManager = new PartyManager();
 		return partyManager;
 	}
 
