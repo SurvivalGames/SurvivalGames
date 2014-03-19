fef7e348711e1484b87cc8c7d07a7c51e2259c42
Dylan Ryman <dylan@dtr-world.com>
2014-03-18 20:15:13 -0400

    Attempted a map download system

--- src/main/java/com/communitysurvivalgames/thesurvivalgames/managers/MultiWorldManager.java
+++ src/main/java/com/communitysurvivalgames/thesurvivalgames/managers/MultiWorldManager.java
@@ -5,22 +5,16 @@
  */
 package com.communitysurvivalgames.thesurvivalgames.managers;
 
-import com.communitysurvivalgames.thesurvivalgames.multiworld.SGWorld;
-import com.communitysurvivalgames.thesurvivalgames.util.UnTAR;
-import org.apache.commons.compress.archivers.ArchiveException;
-import org.apache.commons.io.FileUtils;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bukkit.Bukkit;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FilenameFilter;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
+import com.communitysurvivalgames.thesurvivalgames.multiworld.SGWorld;
 
 public class MultiWorldManager {
     
@@ -44,38 +38,8 @@ public class MultiWorldManager {
         }
     }
 
-    public World copyFromInternet(final Player sender, final String worldName, final String display) {// TODO:
-                                                                                // Translate
-        String url = "http://communitysurvivalgames.com/worlds/" + worldName + ".zip";
-        /*
-         * if (!FileUtil.exists(url)) {
-         * sender.sendMessage("That arena dosen't seem to be in our database!  :("
-         * ); sender.sendMessage(
-         * "Look for worlds we do have at: http://communitysurvivalgames.com/worlds/"
-         * ); return null; }
-         */
-        try {
-            FileUtils.copyURLToFile(new URL(url), new File(SGApi.getPlugin().getDataFolder().getAbsolutePath(), "SG_ARENA_TMP.tar"));
-        } catch (MalformedURLException e) {
-           sender.sendMessage("Bad world name! Are you using special characters?");
-            return null;
-        } catch (IOException e) {
-            sender.sendMessage("World downloading failed, try again later or something");
-            return null;
-        }
-        try {
-            UnTAR.unTar(new File(SGApi.getPlugin().getDataFolder(), "SG_ARENA_TMP.tar"), new File(Bukkit.getServer().getWorldContainer().getAbsolutePath(), worldName));
-      } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ArchiveException e) {
-            e.printStackTrace();
-        }
-        if (!checkIfIsWorld(new File(Bukkit.getServer().getWorldContainer().getAbsolutePath(), worldName))) {
-            sender.sendMessage("The downloaded world was not a world at all!");
-            return null;
-        }
+    public World copyFromInternet(final Player sender, final String worldName, final String display) {
+    	
         createWorld(worldName, display);
 
         return Bukkit.getWorld(worldName);
