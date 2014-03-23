da28ec2883798480f27589db93e945ba6d806b31
Dylan Ryman <dylan@dtr-world.com>
2014-03-23 14:36:16 -0400

    Getting somewhere...

--- src/main/java/com/communitysurvivalgames/thesurvivalgames/net/WebServer.java
+++ src/main/java/com/communitysurvivalgames/thesurvivalgames/net/WebServer.java
@@ -1,18 +1,22 @@
 package com.communitysurvivalgames.thesurvivalgames.net;
 
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
 
 public class WebServer {
 	public static void load() {
-		Server server = new Server(8080);
+		Server server = new Server();
+		ServerConnector connector = new ServerConnector(server);
+		connector.setPort(8080);
+		server.addConnector(connector);
 
 		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
 		context.setContextPath("/");
 		server.setHandler(context);
 
-		ServletHolder holderEvents = new ServletHolder("sg", SGWebSocketServlet.class);
+		ServletHolder holderEvents = new ServletHolder("ws-plugin", SGWebSocketServlet.class);
 		context.addServlet(holderEvents, "/plugin/*");
 
 		try {
