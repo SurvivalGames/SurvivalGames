15f8e44886954d8251d92e2ee1709b1148f717c5
Dylan Ryman <dylan@dtr-world.com>
2014-03-31 17:05:59 -0400

    Upgraded interface

--- src/main/resources/httdocs/js/websocket.js
+++ src/main/resources/httdocs/js/websocket.js
@@ -1,14 +1,18 @@
 var name = window.location;
 document.session.name.value = name;
 var text = document.session.name.value;
-var ws = new WebSocket("ws://" + window.location.hostname + ":8887/");
+//var ws = new WebSocket("ws://" + window.location.hostname + ":8887/");
 var defaultVolume = 25;
 
 $(function(){
-	$("#jslider").slider({max: 50, min: 0, animate: "fast", value: defaultVolume, slide: function(event, ui) {
-            $('#volumelabel').text("Volume: " + ui.value * 2 + "%");
-            defaultVolume = ui.value;
-        }});
+        $(function () {
+            $("#jslider").slider({
+                value: 100
+            });
+            $("#jslider").on("slidechange", function (event, ui) {
+                BlockParty.change_volume(ui);
+            });
+        });
 });
 var godownmix = new Howl({
 	urls : [ 'sounds/godown.ogg' ]
