fd6f7092206748d94b699602310f7228e6f315ad
Dylan Ryman <dylan@dtr-world.com>
2014-03-31 20:35:23 -0400

    Fixed some bugs

--- sg/js/websocket.js
+++ sg/js/websocket.js
@@ -1,21 +1,18 @@
 var name = window.location;
 document.session.name.value = name;
 var text = document.session.name.value;
-//var ws = new WebSocket("ws://" + window.location.hostname + ":8887/");
+var ws = new WebSocket("ws://" + delineate2(text) + ":8887/");
+var name = delineate(text);
 var defaultVolume = 25;
-
-document.getElementById('player').innerHTML += delineate(text);
 
 var godownmix = new Howl({
 	urls : [ 'sounds/godown.ogg' ]
 })
 
+
 ws.onopen = function() {
-
-	if (name != null) {
-		ws.send("name:" + delineate(text));
-	}
-
+	ws.send("name:" + delineate(text));
+    $('#player').html(delineate(text));
 };
 
 ws.onmessage = function(evt) {
@@ -43,4 +40,10 @@ function delineate(str) {
 	theleft = str.indexOf("=") + 1;
 	theright = str.lastIndexOf("&");
 	return (str.substring(theleft, theright));
+}
+
+function delineate2(str)
+{
+point = str.lastIndexOf("=");
+return(str.substring(point+1,str.length));
 }
