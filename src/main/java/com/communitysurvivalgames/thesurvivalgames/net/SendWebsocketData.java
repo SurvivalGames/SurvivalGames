7631b7556405a0178a7b7e688e2db93e0bb0dc85
Dylan Ryman <dylan@dtr-world.com>
2014-04-01 17:01:47 -0400

    Data sending should be in a new thread

--- src/main/java/com/communitysurvivalgames/thesurvivalgames/net/SendWebsocketData.java
+++ src/main/java/com/communitysurvivalgames/thesurvivalgames/net/SendWebsocketData.java
@@ -15,49 +15,77 @@ import com.communitysurvivalgames.thesurvivalgames.objects.SGArena;
 public class SendWebsocketData {
 	public static Map<String, String> music = new HashMap<String, String>();
 
-	public static void playToPlayer(Player p, String data) {
-		if (WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()) != null) {
-			WebsocketServer.s.sendData(WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()), "sound:" + data);
-		}
-	}
+	public static void playToPlayer(final Player p, final String data) {
+		Bukkit.getScheduler().scheduleSyncDelayedTask(SGApi.getPlugin(), new Runnable() {
 
-	public static void playToArena(SGArena arena, String data) {
-		for (String s : arena.getPlayers()) {
-			Player p = Bukkit.getPlayer(s);
-			if (WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()) != null) {
-				WebsocketServer.s.sendData(WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()), "sound:" + data);
+			@Override
+			public void run() {
+				if (WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()) != null) {
+					WebsocketServer.s.sendData(WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()), "sound:" + data);
+				}
 			}
-		}
+		});
+
 	}
 
-	public static void playToAll(String data) {
-		for (Player p : Bukkit.getOnlinePlayers()) {
-			if (WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()) != null) {
-				WebsocketServer.s.sendData(WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()), "sound:" + data);
+	public static void playToArena(final SGArena arena, final String data) {
+		Bukkit.getScheduler().scheduleSyncDelayedTask(SGApi.getPlugin(), new Runnable() {
+
+			@Override
+			public void run() {
+				for (String s : arena.getPlayers()) {
+					Player p = Bukkit.getPlayer(s);
+					if (WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()) != null) {
+						WebsocketServer.s.sendData(WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()), "sound:" + data);
+					}
+				}
 			}
-		}
+		});
+
 	}
 
-	public static void updateArenaStatusForPlayer(Player p) {
-		PlayerData data = SGApi.getPlugin().getPlayerData(p);
-		WebsocketServer.s.sendData(WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()), "points:" + data.getPoints());
-		WebsocketServer.s.sendData(WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()), "kills:" + data.getKills());
-		WebsocketServer.s.sendData(WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()), "wins:" + data.getWins());
-		WebsocketServer.s.sendData(WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()), "rank:" + data.getRank());
-		SGArena a = null;
-		try {
-			a = SGApi.getArenaManager().getArena(p);
-		} catch (ArenaNotFoundException e) {
-			return;
-		}
-		WebsocketServer.s.sendData(WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()), "arena:" + a.getId());
-		WebsocketServer.s.sendData(WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()), "state:" + a.getState());
-		WebsocketServer.s.sendData(WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()), "max:" + a.getMaxPlayers());
-		WebsocketServer.s.sendData(WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()), "min:" + a.getMinPlayers());
-		WebsocketServer.s.sendData(WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()), "players:" + a.getId());
-		WebsocketServer.s.sendData(WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()), "alive:" + join(a.getPlayers(), "<br>"));
-		WebsocketServer.s.sendData(WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()), "dead:" + join(a.getSpectators(), "<br>"));
-		WebsocketServer.s.sendData(WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()), "specs:" + join(a.getSpectators(), "<br>"));
+	public static void playToAll(final String data) {
+		Bukkit.getScheduler().scheduleSyncDelayedTask(SGApi.getPlugin(), new Runnable() {
+
+			@Override
+			public void run() {
+				for (Player p : Bukkit.getOnlinePlayers()) {
+					if (WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()) != null) {
+						WebsocketServer.s.sendData(WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()), "sound:" + data);
+					}
+				}
+			}
+		});
+
+	}
+
+	public static void updateArenaStatusForPlayer(final Player p) {
+		Bukkit.getScheduler().scheduleSyncDelayedTask(SGApi.getPlugin(), new Runnable() {
+
+			@Override
+			public void run() {
+				PlayerData data = SGApi.getPlugin().getPlayerData(p);
+				WebsocketServer.s.sendData(WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()), "points:" + data.getPoints());
+				WebsocketServer.s.sendData(WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()), "kills:" + data.getKills());
+				WebsocketServer.s.sendData(WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()), "wins:" + data.getWins());
+				WebsocketServer.s.sendData(WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()), "rank:" + data.getRank());
+				SGArena a = null;
+				try {
+					a = SGApi.getArenaManager().getArena(p);
+				} catch (ArenaNotFoundException e) {
+					return;
+				}
+				WebsocketServer.s.sendData(WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()), "arena:" + a.getId());
+				WebsocketServer.s.sendData(WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()), "state:" + a.getState());
+				WebsocketServer.s.sendData(WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()), "max:" + a.getMaxPlayers());
+				WebsocketServer.s.sendData(WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()), "min:" + a.getMinPlayers());
+				WebsocketServer.s.sendData(WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()), "players:" + a.getPlayers().size());
+				WebsocketServer.s.sendData(WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()), "alive:" + join(a.getPlayers(), "<br>"));
+				WebsocketServer.s.sendData(WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()), "dead:" + join(a.getSpectators(), "<br>"));
+				WebsocketServer.s.sendData(WebsocketSessionManager.getSessionManager().getSessionByName(p.getName()), "specs:" + join(a.getSpectators(), "<br>"));
+			}
+		});
+
 	}
 
 	public static String join(List<String> list, String delim) {
