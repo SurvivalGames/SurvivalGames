d43e4b9348e4050cc2de36e91b511eb8e8120259
Dylan Ryman <dylan@dtr-world.com>
2014-04-10 22:35:46 -0400

    Sort of started the player stats room

--- src/main/java/com/communitysurvivalgames/thesurvivalgames/command/subcommands/sg/TestCommand.java
+++ src/main/java/com/communitysurvivalgames/thesurvivalgames/command/subcommands/sg/TestCommand.java
@@ -7,6 +7,7 @@ import org.bukkit.entity.Player;
 
 import com.communitysurvivalgames.thesurvivalgames.command.subcommands.SubCommand;
 import com.communitysurvivalgames.thesurvivalgames.managers.SGApi;
+import com.communitysurvivalgames.thesurvivalgames.util.FakeBlockUtil;
 import com.communitysurvivalgames.thesurvivalgames.util.FireworkUtil;
 
 public class TestCommand implements SubCommand {
@@ -17,6 +18,8 @@ public class TestCommand implements SubCommand {
 			FireworkUtil.getCircleUtil().playFireworkRing(p, FireworkEffect.builder().withColor(Color.FUCHSIA).withFade(Color.BLUE).trail(true).flicker(false).with(Type.BALL).build(), Integer.parseInt(args[1]), Integer.parseInt(args[2]));
 		if (args[0].equalsIgnoreCase("kit"))
 			SGApi.getKitManager().displayDefaultKitSelectionMenu(p);
+		if (args[0].equalsIgnoreCase("sphere"))
+			FakeBlockUtil.createFakeSphere(p);
 	}
 
 }
