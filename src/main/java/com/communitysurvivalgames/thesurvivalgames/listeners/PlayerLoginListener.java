24863aba1a376f5e0af1fd1c152ead45966f7f89
Dylan Ryman <dylan@dtr-world.com>
2014-04-12 14:15:23 -0400

    Forces players out of arena world on login

--- src/main/java/com/communitysurvivalgames/thesurvivalgames/listeners/PlayerLoginListener.java
+++ src/main/java/com/communitysurvivalgames/thesurvivalgames/listeners/PlayerLoginListener.java
@@ -2,12 +2,15 @@ package com.communitysurvivalgames.thesurvivalgames.listeners;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import com.communitysurvivalgames.thesurvivalgames.managers.SGApi;
+import com.communitysurvivalgames.thesurvivalgames.multiworld.SGWorld;
 import com.communitysurvivalgames.thesurvivalgames.objects.PlayerData;
 import com.communitysurvivalgames.thesurvivalgames.util.EconUtil;
 
@@ -33,6 +36,14 @@ public class PlayerLoginListener implements Listener {
 						SGApi.getPlugin().setPlayerData(data);
 					}
 				}
+
+				for (SGWorld world : SGApi.getMultiWorldManager().getWorlds()) {
+					if (event.getPlayer().getWorld().getName().equalsIgnoreCase(world.getName())) {
+						World spawn = Bukkit.getWorld(SGApi.getPlugin().getPluginConfig().getHubWorld());
+						event.getPlayer().teleport(spawn.getSpawnLocation());
+					}
+				}
+
 				SGApi.getPlugin().getTracker().trackEvent("Player Login", event.getPlayer().getName());
 			}
 
